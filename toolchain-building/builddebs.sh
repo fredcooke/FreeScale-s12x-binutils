7ec752e7d9041c81768fa7b1f9a3c866a63c305c
Sean Keys <info@powerefi.com>
2011-03-10 14:22:12 -0700

    updated changelogs and build script

--- toolchain-building/builddebs.sh
+++ toolchain-building/builddebs.sh
@@ -3,15 +3,15 @@ set -e
 
 INPUT=0
 WRKDIR=$(pwd)
-
+BUILDDIR=../../freeems-toolchain-build/
 RELEASE_VER=5
-BUILDDIRBIN=/usr/src/binutils-mc9s12x
+BUILDDIRBIN=${BUILDDIR}/binutils-mc9s12x
 SRCDIRBIN=../binutils-2.20/
-BUILDDIRBINXGATE=/usr/src/binutils-mc9xgate
+BUILDDIRBINXGATE=${BUILDDIR}/binutils-mc9xgate
 SRCDIRBIN=../binutils-2.20/
-BUILDDIRGCC=/usr/src/gcc-mc9s12x
-BUILDDIRNEWLIB=/usr/src/newlib-mc9s12x
-
+BUILDDIRGCC=${BUILDDIR}/gcc-mc9s12x
+BUILDDIRNEWLIB=${BUILDDIR}newlib-mc9s12x
+AUTOINSTALL=NO
 
 function builds12x(){
 rm -Rf $BUILDDIRBIN
@@ -28,9 +28,9 @@ cd $BUILDDIRBIN
 fakeroot debian/rules binary
 
 if [ -f /usr/src/binutils-mc9s12x_2.20-0+${RELEASE_VER}_i386.deb ]; then
-  echo "press enter to install binutils deb package"
-  read 
-  dpkg -i /usr/src/binutils-mc9s12x_2.20-0+${RELEASE_VER}_i386.deb; 
+  echo "binutils-mc9s12x package built successfully"
+  #read 
+  #dpkg -i /usr/src/binutils-mc9s12x_2.20-0+${RELEASE_VER}_i386.deb; 
 else 
   echo "exiting please review output"; 
   read
@@ -53,9 +53,9 @@ cd $BUILDDIRBINXGATE
 fakeroot debian/rules binary
 
 if [ -f /usr/src/binutils-mc9xgate_2.20-0+${RELEASE_VER}_i386.deb ]; then
-  echo "press enter to install binutils deb package"
-  read 
-  dpkg -i /usr/src/binutils-mc9xgate_2.20-0+${RELEASE_VER}_i386.deb; 
+  echo "binutils-mc9xgate packages built successfully"
+  #read 
+  #dpkg -i /usr/src/binutils-mc9xgate_2.20-0+${RELEASE_VER}_i386.deb; 
 else 
   echo "please review output"; 
   read
@@ -82,9 +82,9 @@ cd ${BUILDDIRGCC}/gcc-m68hc1x-3.3.6*
 fakeroot dpkg-buildpackage -b -uc 
 
 if [ -f /usr/src/gcc-mc9s12x/gcc-mc9s12x_3.3.6+3.1+dfsg-3+${RELEASE_VER}_i386.deb ]; then
-  echo "press enter to install gcc deb package"
-  read 
-  dpkg -i /usr/src/gcc-mc9s12x/gcc-mc9s12x_3.3.6+3.1+dfsg-3+${RELEASE_VER}_i386.deb; 
+  echo "gcc-mc9s12x packages built successfully"
+  #read 
+  #dpkg -i /usr/src/gcc-mc9s12x/gcc-mc9s12x_3.3.6+3.1+dfsg-3+${RELEASE_VER}_i386.deb; 
 else 
   echo "error building gcc debian package, please review output"; 
   exit -1; 
@@ -111,18 +111,19 @@ fakeroot debian/rules binary
 cd $WKDIR
 }
 
-read
+mkdir $BUILDDIR
+#read
 builds12x
-read
+#read
 buildxgate
-read
+#read
 buildgcc
-read
+#read
 buildnewlib
-read
+#read
 #read
 exit 0;
-read
+#read
 
 
 
