f8e3c21903b54b3ace4791bc621bf8eaba471db9
David Andruczyk <djandruczyk@yahoo.com>
2011-03-16 20:38:12 -0400

    Added modded gcc source to fix bugs in GCC 3.3.6 and updated hacky build script

--- toolchain-building/builddebs.sh
+++ toolchain-building/builddebs.sh
@@ -1,134 +1,99 @@
 #!/bin/bash
-set -e
 
-INPUT=0
-WRKDIR=$(pwd)
-BUILDDIR=../../freeems-toolchain-build/
-RELEASE_VER=5
-BUILDDIRBIN=${BUILDDIR}/binutils-mc9s12x
-SRCDIRBIN=../binutils-2.20/
-BUILDDIRBINXGATE=${BUILDDIR}/binutils-mc9xgate
-SRCDIRBIN=../binutils-2.20/
-BUILDDIRGCC=${BUILDDIR}/gcc-mc9s12x
-BUILDDIRNEWLIB=${BUILDDIR}newlib-mc9s12x
-AUTOINSTALL=NO
 
-function builds12x(){
-rm -Rf $BUILDDIRBIN
-mkdir $BUILDDIRBIN
-tar -cjf ${BUILDDIRBIN}/binutils-2.20.tar.bz2 $SRCDIRBIN
+#
+#  Makes Debs for Jaunty, Karmic, Lucid, Maverick and Lenny
+#
 
-cp -R binutils-s12x/debian $BUILDDIRBIN
-cp -R binutils-s12x/example $BUILDDIRBIN
-#cp -R binutils-s12x/patches $BUILDDIRBIN
+WORKDIR=`pwd`
+BINUTILS="${WORKDIR}"/binutils-2.20.tar.bz2
+OUTDIR="${WORKDIR}"/Output
+OTHERMIRROR=/var/www/repo
+BUILDDIR="${WORKDIR}"/build
+NEWLIBDIR=newlib-1.18.0
+BINUTILSPKGS="binutils-s12x binutils-xgate"
+GCCTARBALL="gcc-mc9s12x_3.3.6+3.1+dfsg-3+7.tar.gz"
 
-cd $BUILDDIRBIN
-#fakeroot debian/rules clean
-#fakeroot debian/rules build
-fakeroot debian/rules binary
-
-if [ -f ${BUILDDIRBIN}/binutils-mc9s12x_2.20-0+${RELEASE_VER}_i386.deb ]; then
-  echo "binutils-mc9s12x package built successfully"
-  #read 
-  dpkg -i ${BUILDDIRBIN}/binutils-mc9s12x_2.20-0+${RELEASE_VER}_i386.deb; 
-else 
-  echo "exiting please review output"; 
-  read
-  exit -1; 
-fi
-cd $WRKDIR
+# Builds the deb pkgs.  Assumes pdebuild has been setup and configured
+# previously and has the rootimages setup for the distros specified
+#
+function build_debs {
+#DEB_RELEASES="lenny jaunty karmic lucid"
+DEB_RELEASES="lucid"
+for dist in `echo "${DEB_RELEASES}"` ; do
+	echo "Building for Distro $dist"
+	DESTDIR="${OUTDIR}"/"${dist}"
+	if [ ! -d "${DESTDIR}" ] ; then
+		mkdir -p "${DESTDIR}"
+	fi
+	find ${OTHERMIRROR}  -type f -exec rm -f {} \;
+	find ${DESTDIR} -type f -name "*.deb" -exec cp -a {} ${OTHERMIRROR} \;
+#pdebuild --buildresult "${DESTDIR}" --pbuilderroot "sudo DIST=${dist}" -- --othermirror "deb file:///${DESTDIR} ./"
+	pdebuild --buildresult "${DESTDIR}" --pbuilderroot "sudo DIST=${dist}"
+	if [ $? -ne 0 ] ; then
+		echo "Build failure for dist $dist"
+		exit -1
+	fi
+	find ${OTHERMIRROR}  -type f -exec rm -f {} \;
+done
 }
 
 
-function buildxgate(){
-rm -Rf $BUILDDIRBINXGATE
-mkdir $BUILDDIRBINXGATE
-tar -cjf ${BUILDDIRBINXGATE}/binutils-2.20.tar.bz2 $SRCDIRBIN
-
-cp -R binutils-xgate/debian $BUILDDIRBINXGATE
-cp -R binutils-xgate/example $BUILDDIRBINXGATE
-#cp -R binutils-xgate/patches $BUILDDIRBINXGATE
-
-cd $BUILDDIRBINXGATE
-fakeroot debian/rules binary
-
-if [ -f ${BUILDDIRBINXGATE}/binutils-mc9xgate_2.20-0+${RELEASE_VER}_i386.deb ]; then
-  echo "binutils-mc9xgate packages built successfully"
-  #read 
-  dpkg -i ${BUILDDIRBINXGATE}/binutils-mc9xgate_2.20-0+${RELEASE_VER}_i386.deb; 
-else 
-  echo "please review output"; 
-  read
-#  exit -1; 
-fi
-cd $WRKDIR
-}
-
-
-function buildgcc {
-# Build GCC with new binutils #
-rm -Rf $BUILDDIRGCC
-mkdir $BUILDDIRGCC
-apt-get source gcc-m68hc1x
-mv gcc-* $BUILDDIRGCC
-
-
-cp gcc/rules ${BUILDDIRGCC}/gcc-m68hc1x-3.3.6*/debian/rules
-cp gcc/control ${BUILDDIRGCC}/gcc-m68hc1x-3.3.6*/debian/control
-cp gcc/changelog ${BUILDDIRGCC}/gcc-m68hc1x-3.3.6*/debian/changelog
-
-cd ${BUILDDIRGCC}/gcc-m68hc1x-3.3.6*
-#fakeroot debian/rules binary
-fakeroot dpkg-buildpackage -b -uc 
-
-if [ -f ${BUILDDIRGCC}/gcc-mc9s12x_3.3.6+3.1+dfsg-3+${RELEASE_VER}_i386.deb ]; then
-  echo "gcc-mc9s12x packages built successfully"
-  #read 
-  dpkg -i ${BUILDDIRGCC}/gcc-mc9s12x_3.3.6+3.1+dfsg-3+${RELEASE_VER}_i386.deb; 
-else 
-  echo "error building gcc debian package, please review output"; 
-  exit -1; 
+# Need to make multiple versions of binutils, 
+# one for S12x, and one for Xgate
+function build_binutils {
+if [ ! -f "${BINUTILS}" ] ; then
+	pushd ..
+	tar cvzf "${BINUTILS}" binutils-2.20
+	popd
 fi
 
-cd $WRKDIR
+for pkg in `echo "${BINUTILSPKGS}"` ; do
+	mkdir -p "${BUILDDIR}"/"${pkg}"
+	cp -a "${pkg}"/* "${BUILDDIR}"/"${pkg}"
+	cp -a "${WORKDIR}"/binutils-2.20.tar.bz2 "${BUILDDIR}"/"${pkg}"
+	pushd "${BUILDDIR}"/"${pkg}"
+	build_debs 
+	popd
+done
+return $?
 }
 
-function buildnewlib {
-# Build GCC with new binutils #
-rm -Rf $BUILDDIRNEWLIB
-mkdir $BUILDDIRNEWLIB
-apt-get source newlib-m68hc1x
-mv newlib-* $BUILDDIRNEWLIB
-mv newlib_* $BUILDDIRNEWLIB
-
-cp newlib/rules ${BUILDDIRNEWLIB}/newlib-1.18.0/debian/rules
-cp newlib/control ${BUILDDIRNEWLIB}/newlib-1.18.0/debian/control
-cp newlib/changelog ${BUILDDIRNEWLIB}/newlib-1.18.0/debian/changelog
-
-cd ${BUILDDIRNEWLIB}/newlib-1.18.0
-fakeroot debian/rules binary
-
-cd $WKDIR
+# Need to make GCC next
+function build_gcc {
+if [ ! -f "${WORKDIR}"/"${GCCTARBALL}" ] ; then
+	echo "MISSING GCC tarball ${GCCTARBALL}"
+	exit -1
+fi
+mkdir -p "${BUILDDIR}"/gcc
+pushd gcc
+tar xfz "${WORKDIR}"/"${GCCTARBALL}"
+pushd gcc-m68hc1x-3.3.6+3.1+dfsg
+cp -a "${WORKDIR}"/gcc/* ./debian
+build_debs
+popd
+popd
+return $?
 }
 
-mkdir $BUILDDIR
-#read
-builds12x
-#read
-buildxgate
-#read
-buildgcc
-#read
-buildnewlib
-#read
-#read
-exit 0;
-#read
+# Need to make Newlib
+function build_newlib {
+pushd "${BUILDDIR}"
+apt-get source newlib-source
+pushd "${NEWLIBDIR}"
+cp -a "${WORKDIR}"/newlib/* ./debian
+build_debs
+popd
+popd
+return $?
+}
 
+function clean {
+rm  -f "${BINUTILS}"
+rm -rf "${BUILDDIR}"
+}
 
-
-
-
-
-
+build_binutils
+build_gcc
+build_newlib
 
