177ee070f2be1bcafd4c0987915b73104dff3ffe
Sean Keys <skeys@ipdatasys.com>
2012-01-27 20:22:31 -0700

    fixed copywrite years

--- src/gas/config/tc-xgate.c
+++ src/gas/config/tc-xgate.c
@@ -1,5 +1,5 @@
 /* tc-xgate.c -- Assembler code for Freescale XGATE
- Copyright 1999, 2000, 2001, 2002, 2004, 2005, 2006, 2007, 2008, 2009, 2010
+ Copyright 2009, 2010, 2011, 2012
  Free Software Foundation, Inc.
  Contributed by Sean Keys <skeys@ipdatasys.com>
 
