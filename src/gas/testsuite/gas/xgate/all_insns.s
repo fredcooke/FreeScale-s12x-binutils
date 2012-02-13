31ecfdbf7789a27948194bf586ccbb232409eefb
Sean Keys <skeys@ipdatasys.com>
2012-02-13 06:27:36 -0700

    Added a test to the gas testsuite that checks all instructions(Assemble/Disassemble).

--- src/gas/testsuite/gas/xgate/all_insns.s
+++ src/gas/testsuite/gas/xgate/all_insns.s
@@ -35,77 +35,77 @@ L30:    ble  END_CODE
 L31:    blo END_CODE
 L32:    bls END_CODE
 L33:	blt END_CODE
-L24:	bmi END_CODE
-L25:    bne END_CODE
-L26:    bpl END_CODE
-L27:    bra END_CODE
-L28:    brk
-L29:    bvc END_CODE
-L30:    bvs END_CODE
-L31:    cmp r1, r2
-L32:    cmpl r3, #255
-L33:    com r4, r5
-L34:    cpc r6, r7
-L35:    cmp r1, #65535
-L36:    cpch r2, #255
-L37:    csem #4
-L38:    csem r5
-L39:    csl r6, #11
-L40:    csl r7, r0
-L41:    csr r1, #2
-L42:    csr r2, r3
-L43:    jal r4
-L44:    ldb r5, (r6, #20)
-L45:    ldb r7, (r0, r1+)
-L46:    ldb r7, (r0, -r1)
-L47:    ldb r0, (r0, r0)
-L48:    ldh r1, #255
-L49:    ldl r2, #255
-L50:	ldd r3, END_CODE
-L51:    ldw r4, (r5, #20)
-L52:    ldw r5, (r6, r7+)
-L53:    ldw r5, (r6, -r7)
-L54:    ldw r1, (r2, r4)
-L55:    lsl r1, #4
-L56:    lsl r2, r3
-L57:    lsr r4, #5
-L58:    lsr r5, r6
-L59:    mov r6, r7
-L60:    neg r1, r2
-L61:    nop
-L62:    or r1, r2, r3
-L63:    orh r4, #255
-L64:    orl r5, #255
-L65:    par r6
-L66:    rol r7, #6
-L67:    rol r1, r2
-L68:    ror r3, #5
-L69:    ror r4, r5
-L70:    rts
-L71:    sbc r1, r2, r3
-L72:    ssem #4
-L73:    ssem r1
-L74:    sex r2
-L75:    sif
-L76:    sif r4
-L77:    stb r5, (r6, #5)
-L78:    stb r0, (r0, r0+)
-L79:    stb r0, (r0, -r0)
-L80:    stb r2, (r0, r0)
-L81:    stw r1, (r2, #16)
-L82:    stw r1, (r2, r3+)
-L83:    stw r1, (r2, -r3)
-L84:    stw r2, (r3 ,r4)
-L85:    sub r3, r4, r6
-L86:    sub r4, #65535
-L87:    subh r5, #255
-L88:    subl r6, #255
-L89:    tfr r7, pc
-L90:    tfr r7,ccr
-L91:    tfr ccr, r7
-L92:    tst r1
-L93:    xnor r1, r2, r3
-L94:    xnorh r4, #255
-L95:    xnorl r5, #255  
+L34:	bmi END_CODE
+L35:    bne END_CODE
+L36:    bpl END_CODE
+L37:    bra END_CODE
+L38:    brk
+L39:    bvc END_CODE
+L40:    bvs END_CODE
+L41:    cmp r1, r2
+L42:    cmpl r3, #255
+L43:    com r4, r5
+L44:    cpc r6, r7
+L45:    cmp r1, #65535
+L46:    cpch r2, #255
+L47:    csem #4
+L48:    csem r5
+L49:    csl r6, #11
+L50:    csl r7, r0
+L51:    csr r1, #2
+L52:    csr r2, r3
+L53:    jal r4
+L54:    ldb r5, (r6, #20)
+L55:    ldb r7, (r0, r1+)
+L56:    ldb r7, (r0, -r1)
+L57:    ldb r0, (r0, r0)
+L58:    ldh r1, #255
+L59:    ldl r2, #255
+L60:	ldd r3, END_CODE
+L61:    ldw r4, (r5, #20)
+L62:    ldw r5, (r6, r7+)
+L63:    ldw r5, (r6, -r7)
+L64:    ldw r1, (r2, r4)
+L65:    lsl r1, #4
+L66:    lsl r2, r3
+L67:    lsr r4, #5
+L68:    lsr r5, r6
+L69:    mov r6, r7
+L70:    neg r1, r2
+L71:    nop
+L72:    or r1, r2, r3
+L73:    orh r4, #255
+L74:    orl r5, #255
+L75:    par r6
+L76:    rol r7, #6
+L77:    rol r1, r2
+L78:    ror r3, #5
+L79:    ror r4, r5
+L80:    rts
+L81:    sbc r1, r2, r3
+L82:    ssem #4
+L83:    ssem r1
+L84:    sex r2
+L85:    sif
+L86:    sif r4
+L87:    stb r5, (r6, #5)
+L88:    stb r0, (r0, r0+)
+L89:    stb r0, (r0, -r0)
+L90:    stb r2, (r0, r0)
+L91:    stw r1, (r2, #16)
+L92:    stw r1, (r2, r3+)
+L93:    stw r1, (r2, -r3)
+L94:    stw r2, (r3 ,r4)
+L95:    sub r3, r4, r6
+L96:    sub r4, #65535
+L97:    subh r5, #255
+L98:    subl r6, #255
+L99:    tfr r7, pc
+L100:   tfr r7,ccr
+L101:   tfr ccr, r7
+L102:   tst r1
+L103:   xnor r1, r2, r3
+L104:   xnorh r4, #255
+L105:   xnorl r5, #255  
 END_CODE:
 
